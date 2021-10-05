a7fef86e8fb74a82b07e0e9e11bb04834a40983a
ShinigamiBoss <fabio.feliciosi@hotmail.it>
2021-10-05 15:55:48 +0200

    Development moved forward, need to implement feedback

--- Q-Sys SY-MSUHD.lua
+++ Q-Sys SY-MSUHD.lua
@@ -1,5 +1,3 @@
---require("QSYS Lua Extension")
-
 PluginInfo = {
     Name = "SY Electronics~MSUHD Series",
     Version = "1.0.0-master",
@@ -9,13 +7,14 @@ PluginInfo = {
     Author = "Fabio Feliciosi"
 }
 
--- We can let users determine some of the plugin properties by exposing them here
--- While this function can be very useful, it is completely optional and not always needed.
--- If no Properties are set here, only the position and fill properties of your plugin will show in the Properties pane
+ButtonSize = {50,30}
+
+
+
 function GetProperties()
     props = {
         {
-            Name = "IP Adress",
+            Name = "IP Address",
             Type = "string",
             Value = "192.168.1.239"
         },
@@ -43,49 +42,170 @@ function GetProperties()
         {
             Name = "On In Color",
             Type = "string",
-            Value = "#FFDF0024"
+            Value = "#DF0024"
         },
         {
             Name = "Off In Color",
             Type = "string",
-            Value = "#FF6C000D"
+            Value = "#6C000D"
         },
         {
             Name = "On Out Color",
             Type = "string",
-            Value = "#FFF289AE"
+            Value = "#F289AE"
         },
         {
             Name = "Off Out Color",
             Type = "string",
-            Value = "#FF754053"
+            Value = "#754053"
         }
     }
     return props
 end
 
--- The below function is where you will populate the controls for your plugin.
--- If you've written some of the Runtime code already, simply use the control names you populated in Text Controller/Control Script, and use their Properties to inform the values here
--- ControlType can be Button, Knob, Indicator or Text
--- ButtonType ( ControlType == Button ) can be Momentary, Toggle or Trigger
--- IndicatorType ( ControlType == Indicator ) can be Led, Meter, Text or Status
--- ControlUnit ( ControlType == Knob ) can be Hz, Float, Integer, Pan, Percent, Position or Seconds
 function GetControls(props)
-    ctls = {
+    return {
         {
-            Name = "InputButton",
+            Name = "In",
             ControlType = "Button",
-            ButtonType = "Trigger",
+            ButtonType = "Toggle",
             Count = props["Input #"].Value,
-            UserPin = true
+            DefautlValue = 8
+
         },
         {
-            Name = "InputButton",
+            Name = "Out",
             ControlType = "Button",
-            ButtonType = "Trigger",
+            ButtonType = "Toggle",
             Count = props["Output #"].Value,
-            UserPin = true
+            DefautlValue = 8
+        }
+    }
+end
+
+
+function GetControlLayout(props)
+
+    local function hex2rgb (hex)
+        local hex = hex:gsub("#","")
+        local r, g, b = tonumber("0x"..hex:sub(1,2)), tonumber("0x"..hex:sub(3,4)), tonumber("0x"..hex:sub(5,6))
+        return {r, g, b}
+    end
+
+    -- layout holds representaiton of Controls
+    local layout = {}
+
+    local graphics = {}
+
+    for x=1,props["Input #"].Value do
+        layout["In "..x] = {
+            Size = ButtonSize,
+            Style = "Button",
+            ButtonStyle = "Toggle",
+            Position = { ButtonSize[1]*(x - 1) + 50, 10 },
+            Color = hex2rgb(props["On In Color"].Value),
+            UnlinkOffColor = true,
+            OffColor = hex2rgb(props["Off In Color"].Value)
+        }
+    end
+
+    for x=1,props["Output #"].Value do
+        layout["Out "..x] = {
+            Style = "Button",
+            ButtonStyle = "Toggle",
+            Position = { ButtonSize[1]*(x - 1) + 50, 40 },
+            Size = ButtonSize,
+            Color =hex2rgb( props["On Out Color"].Value),
+            UnlinkOffColor = true,
+            OffColor = hex2rgb(props["Off Out Color"].Value)
+        }
+    end
+
+    graphics = {
+        {
+            Type = "Label",
+            Text = "Input",
+            HTextAlign = "Left",
+            Position = { 10, 10 },
+            Size = { 40, ButtonSize[2] }
+        },
+        {
+            Type = "Label",
+            Text = "Output",
+            HTextAlign = "Left",
+            Position = { 10, 40 },
+            Size = { 40, ButtonSize[2] }
         },
     }
-    return ctls
+
+    return layout, graphics;
+end
+
+
+function MutuallyExclude(val, on)
+    for index, value in ipairs(val) do
+        value.Boolean = false
+    end
+    val[on].Boolean = true
+end
+
+--Communication variables
+SetOut = "SET OUT"
+SetIn = " VS IN"
+GetStatus = "GET STA\r"
+
+if Controls then
+
+    sock = TcpSocket.New()
+    sock.ReconnectTimeout = 5
+
+    function SendCommand(command)
+        if sock.IsConnected == true then
+            sock:Write(command.."\r")
+        else
+            sock:Connect(Properties["IP Address"].Value, Properties["Port"].Value)
+            Timer.CallAfter(function() sock:Write(command.."\r") end, 1)
+        end
+    end
+
+    --Defautl input selection is 1
+    SelectedInput = 1
+
+    Inputs = {}
+
+    for x=1, Properties["Input #"].Value do
+        Inputs[x] = Controls["In"][x]
+
+        Inputs[x].EventHandler = function()
+            SelectedInput = x
+            MutuallyExclude(Inputs,x)
+        end
+    end
+
+    MutuallyExclude(Inputs, SelectedInput)
+
+    Outputs = {}
+
+    for x=1, Properties["Output #"].Value do
+        Outputs[x] = Controls["Out"][x]
+
+        Outputs[x].EventHandler = function()
+            Command = SetOut..x..SetIn..SelectedInput
+            SendCommand(Command)
+        end
+    end
+    print(Properties["IP Address"].Value)
+
+    sock:Connect(Properties["IP Address"].Value, Properties["Port"].Value)
+
+    feedback = Timer:New()
+    feedback.EventHandler = function()
+        SendCommand(GetStatus)
+    end
+
+    feedback:Start(1)
+
+    sock.Data = function()
+        --TODO Handle Feedback
+    end
 end
